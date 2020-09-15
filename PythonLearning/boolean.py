6f3f77602c05d45bc602d896d7e4f99947b263c8
Diana-nyamai <dnyamai.dn@gmail.com>
2020-09-15 22:36:38 +0300

    first commit

---
+++ PythonLearning/boolean.py
@@ -0,0 +1,15 @@
+is_cold = True
+is_hot = False
+
+if is_cold:
+    print('the weather is cold')
+    print('please wear warm clothes')
+
+elif is_hot:
+    print('the weather is warm')
+    print('dont wear heavy clothes')
+
+else:
+    print('the day is a lovely one')
+
+print('have a lovely day')
---
+++ emojiConverter/emojiConverter.py
@@ -0,0 +1,11 @@
+message = input('>')
+words = message.split(' ')
+emojis = {
+    ":)" : "😀",
+    ":(" : "☹"
+}
+
+output = ""
+for word in words:
+   output += emojis.get(word, word) + ""
+print(output)
