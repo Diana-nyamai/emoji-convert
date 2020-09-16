f2ae672c1b3315a20c2aa0dc98853e5cb433819b
Diana-nyamai <dnyamai.dn@gmail.com>
2020-09-17 00:02:00 +0300

    second commit

--- emojiConverter/emojiconv.py
+++ emojiConverter/emojiconv.py
@@ -1,11 +1,15 @@
-message = input('>')
-words = message.split(' ')
-emojis = {
-    ":)" : "😀",
-    ":(" : "☹"
-}
+def emoji_converter(message):
+    words = message.split(' ')
+    emojis = {
+        ":)" : "😀",
+        ":(" : "☹",
+        ";)" : "😉"
+    }
 
-output = ""
-for word in words:
-   output += emojis.get(word, word) + ""
-print(output)
+    output = ""
+    for word in words:
+       output += emojis.get(word, word) + ""
+    return output
+
+message = input("> ")
+print(emoji_converter(message))
